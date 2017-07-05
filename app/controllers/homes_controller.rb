4599dd9595ce568fa9e3e52921c21a63ed007409
TKim <rlaxodhksk@naver.com>
2017-07-06 07:35:01 +0900

    skyscanner

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,13 @@
 class HomesController < ApplicationController
     def index
     end
-    def contact
+    def skyscanner
+	@dept_date = params[:dept_date].to_s
+	@arrv_date = params[:arrv_date].to_s
+	@arrv_city = params[:arrv_city].to_s
+	@link = "https://www.skyscanner.co.kr/transport/flights/icn/mad/"+@dept_date+"/"+@arrv_date+"/airfares-from-seoul-incheon-int-l-to-"+@arrv_city+"-in-july-2017.html?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&qp_prevProvider=ins_browse&qp_prevCurrency=KRW#results"
+
+
+	redirect_to @link
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :homes
   root "homes#index"
-  get '/contact' => "homes#contact"
+  get '/skyscanner' => "homes#skyscanner"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
